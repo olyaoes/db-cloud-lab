8563a0816dcbba56abd8b241b1efe84000d846b9
olyaoes <151739763+olyaoes@users.noreply.github.com>
2025-11-12 20:07:43 +0200

    Refactor database configuration to use environment variables for better flexibility

--- db-laba/config.py
+++ db-laba/config.py
@@ -7,9 +7,9 @@ os.environ['LANG'] = 'en_US.UTF-8'
 sys.dont_write_bytecode = True
 
 class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'flask_user'
-    MYSQL_PASSWORD = '210lya0822'
-    MYSQL_DB = 'solar_station'
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'flask_user')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '210lya0822')
+    MYSQL_DB = os.getenv('MYSQL_DB', 'solar_station')
     MYSQL_CURSORCLASS = 'DictCursor'
 

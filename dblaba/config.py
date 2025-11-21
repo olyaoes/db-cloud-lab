404852d90d1513810e1232f8ae198622ead9156b
olyaoes <151739763+olyaoes@users.noreply.github.com>
2025-11-21 19:22:22 +0200

    Update database configuration in config.py for local development

--- dblaba/config.py
+++ dblaba/config.py
@@ -5,9 +5,8 @@ os.environ['LANG'] = 'en_US.UTF-8'
 sys.dont_write_bytecode = True
 
 class Config:
-    MYSQL_HOST = os.getenv('MYSQL_HOST', '21server-db.mysql.database.azure.com')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'labolya21@21server-db')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '21Olya0822')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'solar_station')
+    MYSQL_HOST = os.getenv('MYSQL_HOST')
+    MYSQL_USER = os.getenv('MYSQL_USER')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_DB = os.getenv('MYSQL_DB')
     MYSQL_CURSORCLASS = 'DictCursor'
-    MYSQL_PORT = 3306

1ac53fe0edecddb86c910577af52a2efa32facfe
olyaoes <151739763+olyaoes@users.noreply.github.com>
2025-11-21 19:01:54 +0200

    Update database configuration in config.py for production environment

--- dblaba/config.py
+++ dblaba/config.py
@@ -1,15 +1,13 @@
-
 import os
 import sys
 
 os.environ['LANG'] = 'en_US.UTF-8'
-
 sys.dont_write_bytecode = True
 
 class Config:
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'flask_user')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '210lya0822')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', '21server-db.mysql.database.azure.com')
+    MYSQL_USER = os.getenv('MYSQL_USER', 'labolya21@21server-db')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '21Olya0822')
     MYSQL_DB = os.getenv('MYSQL_DB', 'solar_station')
     MYSQL_CURSORCLASS = 'DictCursor'
-
+    MYSQL_PORT = 3306
